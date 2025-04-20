11b8b775c85483356bb831a5617c1f042543edc4
AlexDev <minino-forever@rambler.ru>
2025-04-20 10:47:18 +0700

    Created namespaces StudyOPP

---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleClassExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
