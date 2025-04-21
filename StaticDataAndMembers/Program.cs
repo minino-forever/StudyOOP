fe716f211f889c7302af25c5112d684a1d963586
AlexDev <minino-forever@rambler.ru>
2025-04-21 20:44:36 +0700

    created project StaticDataAndMembers

---
+++ StaticDataAndMembers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace StaticDataAndMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Fun with Static Data*\n");
+
+            SavingAccount s1 = new SavingAccount(50);
+
+            SavingAccount s2 = new SavingAccount(100);
+
+            Console.WriteLine($"Interest Rate is: {SavingAccount.GetInterestRate()}");
+
+            SavingAccount.SetInterestRate(0.08);
+
+            SavingAccount s3 = new SavingAccount(10000.75);
+
+            Console.WriteLine($"Interest Rate is: {SavingAccount.GetInterestRate()}");
+
+            Console.WriteLine("\n*****Fun with static Class*****\n");
+
+            TimeUtilClass.PrintDate();
+
+            TimeUtilClass.PrintTime();
+        }
+    }
+}
