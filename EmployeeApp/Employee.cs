79c84a1bedceb97d2b3ef926d7afab784c3aeaa1
AlexDev <minino-forever@rambler.ru>
2025-04-22 22:11:51 +0700

    created project EmployeeApp

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    internal class Employee
+    {
+        private string _empName;
+
+        private int _empId;
+
+        private float _currPay;
+
+        public string GetName() => _empName;
+
+        public void SetName(string name)
+        {
+            if (name.Length > 15)
+            {
+                Console.WriteLine($"Error! Name lemgth exceeds 15 characters!");
+            }
+            else
+            {
+                _empName = name;
+            }
+        }
+
+        public Employee(){}
+
+        public Employee(string name, int id, float pay)
+        {
+            _empName = name;
+
+            _empId = id;
+
+            _currPay = pay;
+        }
+
+        public void GiveBonus(float amount) => _currPay += amount;
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine($"Nmae: {_empName}");
+
+            Console.WriteLine($"ID: {_empId}");
+
+            Console.WriteLine($"Pay: {_currPay}");
+        }
+    }
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********Fun with Encapsulation******\n");
+
+            Employee emp = new Employee("Marvin", 456, 30000);
+
+            emp.GiveBonus(1000);
+
+            emp.DisplayStatus();
+
+            emp.SetName("Marv");
+
+            Console.WriteLine($"Employee is named: {emp.GetName()}");
+        }
+    }
+}
