f48b1ef0c4e734c1311eb17262a874e0816eba15
AlexDev <minino-forever@rambler.ru>
2025-05-08 18:23:32 +0700

    modified class Car and MiniVan

---
+++ RecordInheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RecordInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Record type inheritance!!****\n");
+
+            Car c = new Car("Honda", "Pilot", "Redd");
+
+            MiniVan m = new MiniVan("Honda", "Pilot", "Redd", 10);
+
+            Console.WriteLine($"Cheking MiniVan is-a Car: {m is Car}");
+        }
+    }
+}
