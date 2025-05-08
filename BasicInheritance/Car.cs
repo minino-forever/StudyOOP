800b9400a51ed8b784d2143291056c7516af4eb6
AlexDev <minino-forever@rambler.ru>
2025-05-08 15:50:02 +0700

    created project BasicInherince

---
+++ BasicInheritance/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    internal class Car
+    {
+        public readonly int MaxSpeed;
+
+        private int _currSpeed;
+
+        public Car()
+        {
+            MaxSpeed = 50;
+        }
+
+        public Car(int max)
+        {
+            MaxSpeed = max;
+        }
+
+        public int Speed
+        {
+            get { return _currSpeed; }
+            set
+            {
+                _currSpeed = value;
+                if (_currSpeed > MaxSpeed)
+                {
+                    _currSpeed = MaxSpeed;
+
+                }
+            }
+        }
+    }
+}
---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Basic Inheritance*******\n");
+
+            Car myCar = new Car(80) {Speed = 50 };
+
+            Console.WriteLine($"My car is going {myCar.Speed}\n");
+        }
+    }
+}
